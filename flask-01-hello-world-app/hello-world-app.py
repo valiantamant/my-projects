67b7c05dd6da01cda479d7c47572bdaaf725cdd3
murat <valiantamant@gmail.com>
2023-05-15 23:24:21 +0300

    update

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -14,4 +14,4 @@ def forth(id):
     return f'Id of this page is {id}'
 if __name__ == '__main__':
     # app.run(debug=True)
-    #app.run(host= '0.0.0.0', port=80)
+    app.run(host= '0.0.0.0', port=80)

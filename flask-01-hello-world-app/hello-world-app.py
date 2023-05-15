f904fe3246a64356b187801bfa94f8b7a5c9fe38
murat <valiantamant@gmail.com>
2023-05-16 00:09:51 +0300

    token utilized

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def head():
-    return 'Her şey güzel olsaydı..'
+    return 'boynumuz eğik..'
 @app.route('/second')
 def second():
     return 'This is second page'
